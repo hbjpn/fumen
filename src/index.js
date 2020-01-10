e04471d813d0dce378c5aa1f903bbb1dbb96ce98
Baba Hiroyuki <hiroyuki.baba.jp@gmail.com>
2020-01-11 04:18:08 +0900

    Change export policy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "@babel/polyfill";
-export * from "./parser/parser";
+export { Parser } from "./parser/parser";
 //export * from './renderer/default_renderer';
-export * from "./renderer/mobile_renderer";
+export { MobileRenderer } from "./renderer/mobile_renderer";
+export { SetupHiDPICanvas } from "./renderer/graphic";

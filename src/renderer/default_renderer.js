a8aa1abea73772023ee9d8e95f85f42db4c6366b
馬場寛之 <>
2020-05-10 19:38:16 +0900

    Default paper size is changed to 375x667 considering smart phones (top share sizes as of 2020)

--- src/renderer/default_renderer.js
+++ src/renderer/default_renderer.js
@@ -21,8 +21,8 @@ import * as presets from "./presets";
  */
 var SR_RENDER_PARAM = {
     // Paper setting
-    paper_width     : (96 * 210) / 25.4, // 96dpi * A4_width[mm] / 25.4[mm/inche], total canvas width = paper_width, internal paper width is paper_width/text_size
-    paper_height    : (96 * 297) / 25.4, // 96dpi * A4_height[mm] / 25.4[mm/inche], total canvas height = paper_height. internal paper height is paper_height/text_size
+    paper_width     : 375, // iPhone 8 etc, the top share as of 2020
+    paper_height    : 667, // iPhone 8 etc, , the top share as of 2020
     text_size       : 1.0, // total canvas size will be [paper_width * text_size, paper_height*text_size]. NOTE that even the canvas size is scaled with text_size, any coordinate unit/size infomation inside the renderer stays the same and no need to be conscious about text_size value.
     pixel_ratio     : 2, // integer. null : use system default, this is not configurable in source as it is memoried in global variable.
     ncol            : 1, // Number of columns of score inside the paper

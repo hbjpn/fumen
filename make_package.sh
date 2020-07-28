acf1e0776c506e69ed8d93e5e4fff4a224b0f0a0
馬場寛之 <>
2020-07-29 01:00:36 +0900

    Add package making script

---
+++ make_package.sh
@@ -0,0 +1,15 @@
+echo "Release versioni (without character v) : "
+read ver
+mkdir release
+cd release
+name="fumen-${ver}"
+mkdir ${name}
+cp ../LICENSE.txt ${name}/
+cp ../OFL.txt ${name}/
+cp ../README.md ${name}/
+cp ../dist/fumen.js ${name}/${name}.js
+cp ../dist/fumen.js.map ${name}/${name}.js.map
+tar cvzf ${name}.tar.gz ${name}
+cd ..
+
+
